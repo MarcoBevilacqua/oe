278c93f48f4fc5fd1be153c636482dddb3a0dce1
Marco <dontstop.marco@gmail.com>
2024-11-18 12:25:05 +0100

    first test + species model setup

--- app/Models/Species.php
+++ app/Models/Species.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Species extends Model
 {
-    //
+
+    protected $table = 'species';
+
+    protected $fillable = ['measures'];
 }
--- tests/Feature/RegionsApiTest.php
+++ tests/Feature/RegionsApiTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class RegionsApiTest extends TestCase
@@ -11,10 +12,11 @@ class RegionsApiTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_should_call_regions_api(): void
     {
-        $response = $this->get('/');
+        Http::fake();
 
-        $response->assertStatus(200);
+        $this->get('/regions')->assertStatus(200)
+            ->assertJsonStructure(['title', 'identifier']);
     }
 }
