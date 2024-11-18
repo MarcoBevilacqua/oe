d94652fbf941e147d4b8c6ad8e6f6b0ae428805c
Marco <dontstop.marco@gmail.com>
2024-11-18 12:00:37 +0100

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Vite::prefetch(concurrency: 3);
+
+        // http client macro
+        Http::macro('iucn', function () {
+            return Http::withHeaders([
+                'X-Example' => 'example',
+            ])->baseUrl(
+                config('api.iucn.base_url') .
+                config('api.iucn.version')
+            )->withQueryParameters(['token' => config('api.iucn.token')]);
+        });
+    }
+}
---
+++ config/api.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'iucn'      => [
+        'base_url'  => env('IUCN_BASE_URL'),
+        'token'     => env('IUCN_API_KEY'),
+        'version'   => env('IUCN_API_VERSION'),
+     ]
+];
