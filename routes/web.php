5f943807adc53a603c81a1de19c0c9cad87ef9b7
Marco <dontstop.marco@gmail.com>
2024-11-18 22:41:46 +0100

    it only defaults to one route (maybe)

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Http\Controllers\RegionController;
 use App\Http\Controllers\SpeciesController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/regions', [RegionController::class, 'index'])->name('regions.index');
-Route::get('/regions/{regionId}', [RegionController::class, 'show'])->name('regions.show');
-
-Route::get('/region/{regionId}/species', [SpeciesController::class, 'index'])->name('regions.edit');
+Route::get('/species/{regionId}', [SpeciesController::class, 'index'])->name('regions.index');
 
 require __DIR__ . '/auth.php';
