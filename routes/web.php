91121bdf8f1330024fe4674cb04b307e662eb4c2
Marco <dontstop.marco@gmail.com>
2024-11-19 15:37:25 +0100

    fix on routes trailing slash

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\SpeciesByCategoryController;
 use App\Http\Controllers\SpeciesByClassController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/species/{regionId}/category/{categoryId}', [SpeciesByCategoryController::class, 'index']);
-Route::get('/species/{regionId}/class/{classId}', [SpeciesByClassController::class, 'index']);
-
+Route::get('/species/{categoryId}/category', [SpeciesByCategoryController::class, 'index']);
+Route::get('/species/{classId}/class', [SpeciesByClassController::class, 'index']);
 
 require __DIR__ . '/auth.php';
